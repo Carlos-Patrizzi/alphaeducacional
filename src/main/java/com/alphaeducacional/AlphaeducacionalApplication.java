1588b4e25a9cec748098787c2e9289f481b59eff
Carlos-Patrizzi <conversecomcarlos@yahoo.com.br>
2022-04-12 20:41:37 -0300

    first commit

---
+++ src/main/java/com/alphaeducacional/AlphaeducacionalApplication.java
@@ -0,0 +1,13 @@
+package com.alphaeducacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaeducacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaeducacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alphaeducacional/AlphaeducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphaeducacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaeducacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
